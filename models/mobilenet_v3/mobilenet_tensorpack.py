6f1c7297f7b3efe6a0e83d6c87f890c913b32e0a
Kevin Stehle <stehle@stud.uni-heidelberg.de>
2020-05-28 23:06:55 +0200

    Added experimental Tensorpack version of MobileNetV3

---
+++ models/mobilenet_v3/mobilenet_tensorpack.py
@@ -0,0 +1,231 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import numpy as np
+import copy
+import functools
+import os
+import sys
+import cv2
+import tensorflow as tf
+from tensorflow.contrib import slim as contrib_slim
+
+from . import conv_blocks as ops
+from . import mobilenet as lib
+
+
+from tensorpack import *
+from tensorpack.dataflow import imgaug
+from tensorpack.tfutils import argscope
+from tensorpack.utils.gpu import get_num_gpu
+
+sys.path.append('../..')
+
+from mpusim_conv2d.mpusim_conv2d_gradient import *
+from mpusim_conv2d.mpusim_conv2d import *
+
+from mpusim_fc.mpusim_mat_mul_gradient import *
+from mpusim_fc.mpusim_fully_connected import *
+
+from models.imagenet_utils import ImageNetModel, get_imagenet_dataflow
+
+def hard_swish(x):
+    with tf.compat.v1.name_scope('hard_swish'):
+        return x*tf.nn.relu6(x + np.float32(3))*np.float32(1./6.)
+    
+def reduce_to_1x1(input_tensor, default_size=7, **kwargs):
+    
+    h, w = input_tensor.shape.as_list()[1:3]
+    if h is not None and w == h:
+        k = [h, h]
+    else:
+        k = [default_size, default_size]
+    return slim.avg_pool2d(input_tensor, kernel_size=k, **kwargs)
+    
+def mbv3_op(input_tensor, ef, n, k, s=1, act=tf.nn.relu, se=None, **kwargs):
+    
+    return op.expanded_conv(input_tensor,
+                                expansion_size=expand_input(ef),
+                                kernel_size=(k, k),
+                                stride=s,
+                                num_outputs=n,
+                                inner_activation_fn=act,
+                                expansion_transform=se,
+                                **kwargs)
+
+# Squeeze Excite with all parameters filled-in, we use hard-sigmoid
+# for gating function and relu for inner activation function.
+squeeze_excite = functools.partial(
+    ops.squeeze_excite, squeeze_factor=4,
+    inner_activation_fn=tf.nn.relu,
+    gating_fn=lambda x: tf.nn.relu6(x+3)*0.16667)
+
+# Wrap squeeze excite op as expansion_transform that takes
+# both expansion and input tensor.
+_se4 = lambda expansion_tensor, input_tensor: squeeze_excite(expansion_tensor)
+
+mbv3_op_se = functools.partial(mbv3_op, se=_se4)
+
+class Model(ImageNetModel):
+    
+    def __init__(self,
+                    data_format='NHWC',
+                    wd=5e-4,
+                    learning_rate=0.1,
+                    activations_datatype_size_byte=1,
+                    weights_datatype_size_byte=1,
+                    results_datatype_size_byte=4,
+                    systolic_array_height=256,
+                    systolic_array_width=256,
+                    accumulator_array_height=4096,
+                    mpusim_logdir=''):
+        super(Model, self).__init__(data_format, wd)
+
+        self.activations_datatype_size_byte=activations_datatype_size_byte
+        self.weights_datatype_size_byte=weights_datatype_size_byte
+        self.results_datatype_size_byte=results_datatype_size_byte
+        self.systolic_array_height=systolic_array_height
+        self.systolic_array_width=systolic_array_width
+        self.accumulator_array_height=accumulator_array_height
+        
+        self.mpusim_logdir=mpusim_logdir
+
+    def get_logits(self, image):
+        constant_init = tf.constant_initializer(1)
+        with argscope([Conv2D, MaxPooling], data_format=self.data_format), \
+                argscope([Conv2D, FullyConnected],
+                                activation=tf.nn.relu,
+                                kernel_initializer=constant_init):
+                
+            
+            l = slim.conv2d(image,
+                                stride=2,
+                                num_outputs=16,
+                                kernel_size=(3, 3),
+                                activation_fn=hard_swish)
+            
+            l = mbv3_op_se(l, ef=1, n=16, k=3, s=2)
+            l = mbv3_op(l, ef=72./16, n=24, k=3, s=2)
+            l = mbv3_op(l, ef=(88./24), n=24, k=3, s=1)
+            l = mbv3_op_se(l, ef=4, n=40, k=5, s=2, act=hard_swish)
+            l = mbv3_op_se(l, ef=6, n=40, k=5, s=1, act=hard_swish)
+            l = mbv3_op_se(l, ef=6, n=40, k=5, s=1, act=hard_swish)
+            l = mbv3_op_se(l, ef=3, n=48, k=5, s=1, act=hard_swish)
+            l = mbv3_op_se(l, ef=3, n=48, k=5, s=1, act=hard_swish)
+            l = mbv3_op_se(l, ef=6, n=96, k=5, s=2, act=hard_swish)
+            l = mbv3_op_se(l, ef=6, n=96, k=5, s=1, act=hard_swish)
+            l = mbv3_op_se(l, ef=6, n=96, k=5, s=1, act=hard_swish)
+            
+            l = slim.conv2d(l, stride=1, kernel_size=[1, 1],
+                                num_outputs=576, activation_fn=hard_swish)
+            
+            l = reduce_to_1x1(l, default_size=7, stride=1, padding='VALID')
+            
+            l = slim.conv2d(l, stride=1, kernel_size=[1, 1],
+                                num_outputs=1024, normalizer_fn=None,
+                                activation_fn=hard_swish)
+            
+            logits = slim.conv2d(net,
+                                    1000,
+                                    [1, 1],
+                                    activation_fn=None,
+                                    normalizer_fn=None,
+                                    biases_initializer=tf.compat.v1.zeros_initializer(),
+                                    scope='Conv2d_1c_1x1')
+
+            return tf.squeeze(logits, [1, 2])
+
+
+def get_data(name, batch):
+    isTrain = name == 'train'
+    if isTrain:
+        augmentors = [
+            imgaug.ResizeShortestEdge(256, cv2.INTER_CUBIC),
+            imgaug.RandomCrop(224),
+            imgaug.Lighting(0.1,
+                            eigval=np.asarray(
+                                [0.2175, 0.0188, 0.0045][::-1]) * 255.0,
+                            eigvec=np.array(
+                                [[-0.5675, 0.7192, 0.4009],
+                                 [-0.5808, -0.0045, -0.8140],
+                                 [-0.5836, -0.6948, 0.4203]],
+                                dtype='float32')[::-1, ::-1]),
+            imgaug.Flip(horiz=True)]
+    else:
+        augmentors = [
+            imgaug.ResizeShortestEdge(256, cv2.INTER_CUBIC),
+            imgaug.CenterCrop((224, 224))]
+    return get_imagenet_dataflow(args.data, name, batch, augmentors)
+
+
+def get_config(activations_datatype_size_byte,
+                weights_datatype_size_byte,
+                results_datatype_size_byte,
+                systolic_array_height,
+                systolic_array_width,
+                accumulator_array_height,
+                mpusim_logdir):
+    
+    nr_tower = max(get_num_gpu(), 1)
+    BASE_LR = 0.01 * (1. / 128.)
+
+    logger.info("Running on {} towers. Batch size per tower: {}".format(nr_tower, 1))
+    
+    data = QueueInput(FakeData(
+            [[1, 224, 224, 3], [1]], 1, random=False, dtype='uint8'))
+    callbacks = []
+
+    return TrainConfig(
+                model=Model(
+                        activations_datatype_size_byte=activations_datatype_size_byte,
+                        weights_datatype_size_byte=weights_datatype_size_byte,
+                        results_datatype_size_byte=results_datatype_size_byte,
+                        systolic_array_height=systolic_array_height,
+                        systolic_array_width=systolic_array_width,
+                        accumulator_array_height=accumulator_array_height,
+                        mpusim_logdir=mpusim_logdir),
+                data=data,
+                callbacks=callbacks,
+                steps_per_epoch=1,
+                max_epoch=1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--activations-datatype-size-byte',
+                            help='activations datatype size in byte',
+                            type=int, default=1)
+    parser.add_argument('--weights-datatype-size-byte',
+                            help='weights datatype size in byte',
+                            type=int, default=1)
+    parser.add_argument('--results-datatype-size-byte',
+                            help='results datatype size in byte',
+                            type=int, default=4)
+    parser.add_argument('--systolic-array-height',
+                            help='systolic array height',
+                            type=int, default=256)
+    parser.add_argument('--systolic-array-width',
+                            help='systolic array width',
+                            type=int, default=256)
+    parser.add_argument('--accumulator-array-height',
+                            help='accumulator array height',
+                            type=int, default=4096)
+    parser.add_argument('--tensorpack-logdir-id', help='TensorPack training log directory id',
+                            type=str, default='')
+    parser.add_argument('--mpusim-logdir', help='MPU simulator log directory',
+                            type=str, default='.')
+    args = parser.parse_args()
+    
+    logger.set_logger_dir(os.path.join('train_log', 'alexnet' + args.tensorpack_logdir_id))
+    
+    config = get_config(args.activations_datatype_size_byte,
+                        args.weights_datatype_size_byte,
+                        args.results_datatype_size_byte,
+                        args.systolic_array_height,
+                        args.systolic_array_width,
+                        args.accumulator_array_height,
+                        args.mpusim_logdir)
+    
+    launch_train_with_config(config, SimpleTrainer()) 
--- models/mobilenet_v3/mobilenet_v3.py
+++ models/mobilenet_v3/mobilenet_v3.py
@@ -45,67 +45,64 @@ _se4 = lambda expansion_tensor, input_tensor: squeeze_excite(expansion_tensor)
 
 
 def hard_swish(x):
-with tf.compat.v1.name_scope('hard_swish'):
-    return x * tf.nn.relu6(x + np.float32(3)) * np.float32(1. / 6.)
+    with tf.compat.v1.name_scope('hard_swish'):
+        return x * tf.nn.relu6(x + np.float32(3)) * np.float32(1. / 6.)
 
 
 def reduce_to_1x1(input_tensor, default_size=7, **kwargs):
-h, w = input_tensor.shape.as_list()[1:3]
-if h is not None and w == h:
-    k = [h, h]
-else:
-    k = [default_size, default_size]
-return slim.avg_pool2d(input_tensor, kernel_size=k, **kwargs)
+    h, w = input_tensor.shape.as_list()[1:3]
+    if h is not None and w == h:
+        k = [h, h]
+    else:
+        k = [default_size, default_size]
+    return slim.avg_pool2d(input_tensor, kernel_size=k, **kwargs)
 
 
 def mbv3_op(ef, n, k, s=1, act=tf.nn.relu, se=None, **kwargs):
-"""Defines a single Mobilenet V3 convolution block.
-Args:
-    ef: expansion factor
-    n: number of output channels
-    k: stride of depthwise
-    s: stride
-    act: activation function in inner layers
-    se: squeeze excite function.
-    **kwargs: passed to expanded_conv
-Returns:
-    An object (lib._Op) for inserting in conv_def, representing this operation.
-"""
-return op(
-    ops.expanded_conv,
-    expansion_size=expand_input(ef),
-    kernel_size=(k, k),
-    stride=s,
-    num_outputs=n,
-    inner_activation_fn=act,
-    expansion_transform=se,
-    **kwargs)
+    """Defines a single Mobilenet V3 convolution block.
+    Args:
+        ef: expansion factor
+        n: number of output channels
+        k: stride of depthwise
+        s: stride
+        act: activation function in inner layers
+        se: squeeze excite function.
+        **kwargs: passed to expanded_conv
+    Returns:
+        An object (lib._Op) for inserting in conv_def, representing this operation.
+    """
+    return op(ops.expanded_conv,
+                expansion_size=expand_input(ef),
+                kernel_size=(k, k),
+                stride=s,
+                num_outputs=n,
+                inner_activation_fn=act,
+                expansion_transform=se,
+                **kwargs)
 
 
 def mbv3_fused(ef, n, k, s=1, **kwargs):
-"""Defines a single Mobilenet V3 convolution block.
-Args:
-    ef: expansion factor
-    n: number of output channels
-    k: stride of depthwise
-    s: stride
-    **kwargs: will be passed to mbv3_op
-Returns:
-    An object (lib._Op) for inserting in conv_def, representing this operation.
-"""
-expansion_fn = functools.partial(slim.conv2d, kernel_size=k, stride=s)
-return mbv3_op(
-    ef,
-    n,
-    k=1,
-    s=s,
-    depthwise_location=None,
-    expansion_fn=expansion_fn,
-    **kwargs)
+    """Defines a single Mobilenet V3 convolution block.
+    Args:
+        ef: expansion factor
+        n: number of output channels
+        k: stride of depthwise
+        s: stride
+        **kwargs: will be passed to mbv3_op
+    Returns:
+        An object (lib._Op) for inserting in conv_def, representing this operation.
+    """
+    expansion_fn = functools.partial(slim.conv2d, kernel_size=k, stride=s)
+    return mbv3_op(ef,
+                    n,
+                    k=1,
+                    s=s,
+                    depthwise_location=None,
+                    expansion_fn=expansion_fn,
+                    **kwargs)
 
 
 mbv3_op_se = functools.partial(mbv3_op, se=_se4)
-
 
 DEFAULTS = {
     (ops.expanded_conv,):
@@ -267,82 +264,95 @@ V3_EDGETPU = dict(
 
 @slim.add_arg_scope
 def mobilenet(input_tensor,
-            num_classes=1001,
-            depth_multiplier=1.0,
-            scope='MobilenetV3',
-            conv_defs=None,
-            finegrain_classification_mode=False,
-            **kwargs):
-"""Creates mobilenet V3 network.
-Inference mode is created by default. To create training use training_scope
-below.
-with tf.contrib.slim.arg_scope(mobilenet_v3.training_scope()):
-    logits, endpoints = mobilenet_v3.mobilenet(input_tensor)
-Args:
-    input_tensor: The input tensor
-    num_classes: number of classes
-    depth_multiplier: The multiplier applied to scale number of
-    channels in each layer.
-    scope: Scope of the operator
-    conv_defs: Which version to create. Could be large/small or
-    any conv_def (see mobilenet_v3.py for examples).
-    finegrain_classification_mode: When set to True, the model
-    will keep the last layer large even for small multipliers. Following
-    https://arxiv.org/abs/1801.04381
-    it improves performance for ImageNet-type of problems.
-    *Note* ignored if final_endpoint makes the builder exit earlier.
-    **kwargs: passed directly to mobilenet.mobilenet:
-    prediction_fn- what prediction function to use.
-    reuse-: whether to reuse variables (if reuse set to true, scope
-    must be given).
-Returns:
-    logits/endpoints pair
-Raises:
-    ValueError: On invalid arguments
-"""
-if conv_defs is None:
-    conv_defs = V3_LARGE
-if 'multiplier' in kwargs:
-    raise ValueError('mobilenetv2 doesn\'t support generic '
-                    'multiplier parameter use "depth_multiplier" instead.')
-if finegrain_classification_mode:
-    conv_defs = copy.deepcopy(conv_defs)
-    conv_defs['spec'][-1] = conv_defs['spec'][-1]._replace(
-        multiplier_func=lambda params, multiplier: params)
-depth_args = {}
-with slim.arg_scope((lib.depth_multiplier,), **depth_args):
-    return lib.mobilenet(
-        input_tensor,
-        num_classes=num_classes,
-        conv_defs=conv_defs,
-        scope=scope,
-        multiplier=depth_multiplier,
-        **kwargs)
+                num_classes=1001,
+                depth_multiplier=1.0,
+                scope='MobilenetV3',
+                conv_defs=None,
+                finegrain_classification_mode=False,
+                **kwargs):
+    
+    """Creates mobilenet V3 network.
+    Inference mode is created by default. To create training use training_scope
+    below.
+    with tf.contrib.slim.arg_scope(mobilenet_v3.training_scope()):
+        logits, endpoints = mobilenet_v3.mobilenet(input_tensor)
+    Args:
+        input_tensor: The input tensor
+        num_classes: number of classes
+        depth_multiplier: The multiplier applied to scale number of
+        channels in each layer.
+        scope: Scope of the operator
+        conv_defs: Which version to create. Could be large/small or
+        any conv_def (see mobilenet_v3.py for examples).
+        finegrain_classification_mode: When set to True, the model
+        will keep the last layer large even for small multipliers. Following
+        https://arxiv.org/abs/1801.04381
+        it improves performance for ImageNet-type of problems.
+        *Note* ignored if final_endpoint makes the builder exit earlier.
+        **kwargs: passed directly to mobilenet.mobilenet:
+        prediction_fn- what prediction function to use.
+        reuse-: whether to reuse variables (if reuse set to true, scope
+        must be given).
+    Returns:
+        logits/endpoints pair
+    Raises:
+        ValueError: On invalid arguments
+    """
+    if conv_defs is None:
+        conv_defs = V3_LARGE
+        
+    if 'multiplier' in kwargs:
+        raise ValueError('mobilenetv2 doesn\'t support generic '
+                        'multiplier parameter use "depth_multiplier" instead.')
+    
+    if finegrain_classification_mode:
+        conv_defs = copy.deepcopy(conv_defs)
+        conv_defs['spec'][-1] = conv_defs['spec'][-1]._replace(
+            multiplier_func=lambda params, multiplier: params)
+        
+    depth_args = {}
+    
+    with slim.arg_scope((lib.depth_multiplier,), **depth_args):
+        return lib.mobilenet(input_tensor,
+                                num_classes=num_classes,
+                                conv_defs=conv_defs,
+                                scope=scope,
+                                multiplier=depth_multiplier,
+                                **kwargs)
 
-mobilenet.default_image_size = 224
-training_scope = lib.training_scope
+    mobilenet.default_image_size = 224
+    training_scope = lib.training_scope
 
 
 @slim.add_arg_scope
 def mobilenet_base(input_tensor, depth_multiplier=1.0, **kwargs):
-"""Creates base of the mobilenet (no pooling and no logits) ."""
-return mobilenet(
-    input_tensor, depth_multiplier=depth_multiplier, base_only=True, **kwargs)
+    
+    """Creates base of the mobilenet (no pooling and no logits) ."""
+    return mobilenet(input_tensor,
+                        depth_multiplier=depth_multiplier,
+                        base_only=True,
+                        **kwargs)
 
 
-def wrapped_partial(func, new_defaults=None,
+def wrapped_partial(func,
+                    new_defaults=None,
                     **kwargs):
-"""Partial function with new default parameters and updated docstring."""
-if not new_defaults:
-    new_defaults = {}
-def func_wrapper(*f_args, **f_kwargs):
-    new_kwargs = dict(new_defaults)
-    new_kwargs.update(f_kwargs)
-    return func(*f_args, **new_kwargs)
-functools.update_wrapper(func_wrapper, func)
-partial_func = functools.partial(func_wrapper, **kwargs)
-functools.update_wrapper(partial_func, func)
-return partial_func
+    
+    """Partial function with new default parameters and updated docstring."""
+    
+    if not new_defaults:
+        new_defaults = {}
+        
+    def func_wrapper(*f_args, **f_kwargs):
+        new_kwargs = dict(new_defaults)
+        new_kwargs.update(f_kwargs)
+        return func(*f_args, **new_kwargs)
+    
+    functools.update_wrapper(func_wrapper, func)
+    partial_func = functools.partial(func_wrapper, **kwargs)
+    functools.update_wrapper(partial_func, func)
+    
+    return partial_func
 
 
 large = wrapped_partial(mobilenet, conv_defs=V3_LARGE)
@@ -365,25 +375,25 @@ small_minimalistic = wrapped_partial(mobilenet, conv_defs=V3_SMALL_MINIMALISTIC)
 
 
 def _reduce_consecutive_layers(conv_defs, start_id, end_id, multiplier=0.5):
-"""Reduce the outputs of consecutive layers with multiplier.
-Args:
-    conv_defs: Mobilenet conv_defs.
-    start_id: 0-based index of the starting conv_def to be reduced.
-    end_id: 0-based index of the last conv_def to be reduced.
-    multiplier: The multiplier by which to reduce the conv_defs.
-Returns:
-    Mobilenet conv_defs where the output sizes from layers [start_id, end_id],
-    inclusive, are reduced by multiplier.
-Raises:
-    ValueError if any layer to be reduced does not have the 'num_outputs'
-    attribute.
-"""
-defs = copy.deepcopy(conv_defs)
-for d in defs['spec'][start_id:end_id+1]:
-    d.params.update({
-        'num_outputs': np.int(np.round(d.params['num_outputs'] * multiplier))
-    })
-return defs
+    """Reduce the outputs of consecutive layers with multiplier.
+    Args:
+        conv_defs: Mobilenet conv_defs.
+        start_id: 0-based index of the starting conv_def to be reduced.
+        end_id: 0-based index of the last conv_def to be reduced.
+        multiplier: The multiplier by which to reduce the conv_defs.
+    Returns:
+        Mobilenet conv_defs where the output sizes from layers [start_id, end_id],
+        inclusive, are reduced by multiplier.
+    Raises:
+        ValueError if any layer to be reduced does not have the 'num_outputs'
+        attribute.
+    """
+    defs = copy.deepcopy(conv_defs)
+    for d in defs['spec'][start_id:end_id+1]:
+        d.params.update({
+            'num_outputs': np.int(np.round(d.params['num_outputs'] * multiplier))
+        })
+    return defs
 
 
 V3_LARGE_DETECTION = _reduce_consecutive_layers(V3_LARGE, 13, 16)
